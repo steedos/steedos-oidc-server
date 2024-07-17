dd04aefd6ced8e5ab0d5e84e6f03417add712989
孙浩林 <sunhaolin@steedos.com>
2024-07-17 19:59:37 +0800

    init

---
+++ express.js
@@ -0,0 +1,84 @@
+/*
+ * @Author: 孙浩林 sunhaolin@steedos.com
+ * @Date: 2024-07-17 16:34:51
+ * @LastEditors: 孙浩林 sunhaolin@steedos.com
+ * @LastEditTime: 2024-07-17 19:14:11
+ * @FilePath: /steedos-oidc-server/express.js
+ * @Description: 
+ */
+/* eslint-disable no-console */
+
+const path = require('path');
+const url = require('url');
+
+const express = require('express'); // eslint-disable-line import/no-unresolved
+const helmet = require('helmet');
+
+const { Provider } = require('oidc-provider'); // require('oidc-provider');
+
+const Account = require('./support/account');
+const configuration = require('./support/configuration');
+const routes = require('./routes/express');
+
+const { PORT = 3000, ISSUER = `http://localhost:${PORT}` } = process.env;
+configuration.findAccount = Account.findAccount;
+
+const app = express();
+
+const directives = helmet.contentSecurityPolicy.getDefaultDirectives();
+delete directives['form-action'];
+app.use(helmet({
+    contentSecurityPolicy: {
+        useDefaults: false,
+        directives,
+    },
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+let server;
+(async () => {
+    let adapter;
+    // process.env.MONGODB_URI = process.env.MONGO_URL
+    if (process.env.MONGODB_URI) {
+        adapter = require('./adapters/mongodb'); // eslint-disable-line global-require
+        await adapter.connect();
+    }
+
+    const prod = process.env.NODE_ENV === 'production';
+
+    const provider = new Provider(ISSUER, { adapter, ...configuration });
+
+    if (prod) {
+        app.enable('trust proxy');
+        provider.proxy = true;
+
+        app.use((req, res, next) => {
+            if (req.secure) {
+                next();
+            } else if (req.method === 'GET' || req.method === 'HEAD') {
+                res.redirect(url.format({
+                    protocol: 'https',
+                    host: req.get('host'),
+                    pathname: req.originalUrl,
+                }));
+            } else {
+                res.status(400).json({
+                    error: 'invalid_request',
+                    error_description: 'do yourself a favor and only use https',
+                });
+            }
+        });
+    }
+
+    routes(app, provider);
+    app.use(provider.callback());
+    server = app.listen(PORT, () => {
+        console.log(require('chalk').green(`application is listening on port ${PORT}, check its /.well-known/openid-configuration`));
+    });
+})().catch((err) => {
+    if (server && server.listening) server.close();
+    console.error(err);
+    process.exitCode = 1;
+});
