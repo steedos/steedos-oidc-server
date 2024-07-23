8af842306fb7c0fe09a2f5cda89cc970ea8f1917
chenzhipei <18605199364@163.com>
2024-07-23 06:03:24 +0000

    update postbuild.sh

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -4,17 +4,16 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
 
-cp -r ./adapters ./.amplify-hosting/compute/default/adapters
-cp -r ./routes ./.amplify-hosting/compute/default/routes
-cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
-cp -r ./support ./.amplify-hosting/compute/default/support
-cp -r ./utils ./.amplify-hosting/compute/default/utils
-cp -r ./views ./.amplify-hosting/compute/default/views
-cp ./.env ./.amplify-hosting/compute/default/.env
-cp ./configuration.json ./.amplify-hosting/compute/default/configuration.json
-cp ./express.js ./.amplify-hosting/compute/default/express.js
-cp ./package.json ./.amplify-hosting/compute/default/package.json
-cp ./yarn.lock ./.amplify-hosting/compute/default/yarn.lock
+cp -r ./adapters ./.amplify-hosting/compute/default
+cp -r ./routes ./.amplify-hosting/compute/default
+cp -r ./node_modules ./.amplify-hosting/compute/default
+cp -r ./utils ./.amplify-hosting/compute/default
+cp -r ./views ./.amplify-hosting/compute/default
+cp ./.env ./.amplify-hosting/compute/default
+cp ./configuration.json ./.amplify-hosting/compute/default
+cp ./express.js ./.amplify-hosting/compute/default
+cp ./package.json ./.amplify-hosting/compute/default
+cp ./yarn.lock ./.amplify-hosting/compute/default
 
 cp -r public ./.amplify-hosting/static
 
