929a88a6134eee84968b19d015d123379098c6e1
chenzhipei <18605199364@163.com>
2024-07-23 07:23:06 +0000

    update postbuild.js

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -4,16 +4,26 @@ rm -rf ./.amplify-hosting
 
 mkdir -p ./.amplify-hosting/compute
 
-cp ./README.md ./.amplify-hosting/compute/default
 cp -r ./routes ./.amplify-hosting/compute/default
+
+cp -r ./routes ./.amplify-hosting/compute/default
+
 cp -r ./node_modules ./.amplify-hosting/compute/default
+
 cp -r ./utils ./.amplify-hosting/compute/default
+
 cp -r ./views ./.amplify-hosting/compute/default
+
 cp -r ./support ./.amplify-hosting/compute/default
+
 cp -r ./adapters ./.amplify-hosting/compute/default
+
 cp ./.env ./.amplify-hosting/compute/default
+
 cp ./index.js ./.amplify-hosting/compute/default
+
 cp ./package.json ./.amplify-hosting/compute/default
+
 cp ./yarn.lock ./.amplify-hosting/compute/default
 
 cp -r public ./.amplify-hosting/static
