0c722525334519c76c4d8c4b39977b9991e111c5
chenzhipei <18605199364@163.com>
2024-07-23 07:05:11 +0000

    update postbuild.js add support

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -9,6 +9,7 @@ cp -r ./routes ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default
 cp -r ./utils ./.amplify-hosting/compute/default
 cp -r ./views ./.amplify-hosting/compute/default
+cp -r ./support ./.amplify-hosting/compute/default
 cp ./.env ./.amplify-hosting/compute/default
 cp ./index.js ./.amplify-hosting/compute/default
 cp ./package.json ./.amplify-hosting/compute/default
